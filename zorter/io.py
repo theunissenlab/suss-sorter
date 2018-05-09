0d144c76946360746c5690a3d2b81f0fcef282f8
kevin <thekevinyu@gmail.com>
2018-05-09 10:07:24 -0700

    Sparse encoding model for spikes

---
+++ zorter/io.py
@@ -0,0 +1,36 @@
+import pickle
+import numpy as np
+import scipy
+
+from .sparse import Dictionary
+
+
+def read_numpy(filename):
+    return np.load(filename)[()]
+
+
+def read_pickle(filename):
+    with open(filename, "rb") as open_file:
+        return pickle.load(open_file)
+
+
+def read_mat(filename):
+    raise IOError(".mat compatibility not supported yet")
+
+
+def load_dictionary(filename):
+    keys = read_numpy(filename)
+    assert "components" in keys
+    assert "sample_rate" in keys
+    assert "center_bin" in keys
+
+    return Dictionary(**keys)
+
+
+def save_dictionary(filename, dictionary):
+    to_save = {
+            "components": dictionary.components,
+            "sample_rate": dictionary.sample_rate,
+            "center_bin": dictionary.center_bin
+    }
+    np.save(filename, to_save)
