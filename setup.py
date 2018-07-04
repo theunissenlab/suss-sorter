5436589caa9a9b7923b15afa1d1b36776ed02c8f
Kevin Yu <thekevinyu@gmail.com>
2018-07-03 22:52:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="suss",
-    version="0.0",
+    version="0.1",
     packages=["suss"],
     description=("Automated spike sorting for single channel electrophysiology"),
     author="Kevin Yu",
