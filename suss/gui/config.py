8fabdc33b3049f9ca04e6c50a9d093af18905bf6
kevin <thekevinyu@gmail.com>
2018-08-29 10:08:32 -0700

    Add annie tag

--- suss/gui/config.py
+++ suss/gui/config.py
@@ -1,11 +1,12 @@
 ISI_MAX = 0.100
 ISI_BINS = 30
-BASE_DIRECTORY = None
+BASE_DIRECTORY = "/auto/fhome/kevin/Projects/songephys/data/birds/GreYel3594M/sites/GreYel3594M___170913_121420/sort_results"
 DEFAULT_SAVE_LOCATION = "../manually_curated"
 
 USERS = [
     "Kevin",
     "Bill",
     "Frederic",
-    "Lily"
+    "Lily",
+    "Annie"
 ]
